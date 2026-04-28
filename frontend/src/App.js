f74705227ab45cd79df9f27c2fb71e84d379a89f
Rajal7777 <suwalrajal57@gmail.com>
2026-04-28 22:51:24 +0900

    login with route protection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,14 +13,33 @@ import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
+import AuthenticationPage, { action as authAction } from './pages/Authentication';
+import { action as logoutAction} from './pages/Logout'
+import { tokenLoader } from './util/auth';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    id: 'root',
+    loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
+      {
+        path: 'newsletter',
+        element: <NewsletterPage />,
+        action: newsletterAction,
+      },
+      {
+        path: 'auth',
+        element: <AuthenticationPage />,
+        action: authAction,
+      },
+      {
+        path: 'logout',
+        action: logoutAction,
+      },
       {
         path: 'events',
         element: <EventsRootLayout />,
@@ -54,11 +73,7 @@ const router = createBrowserRouter([
           },
         ],
       },
-      {
-        path: 'newsletter',
-        element: <NewsletterPage />,
-        action: newsletterAction,
-      },
+
     ],
   },
 ]);
